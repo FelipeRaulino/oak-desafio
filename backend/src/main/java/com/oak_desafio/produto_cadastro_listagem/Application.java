57c1110971d2496f84fb08ea6cba094d2af7709e
Felipe Raulino <felipe.raaulino29@gmail.com>
2024-06-21 17:30:42 -0300

    chore: add backend

---
+++ backend/src/main/java/com/oak_desafio/produto_cadastro_listagem/Application.java
@@ -0,0 +1,13 @@
+package com.oak_desafio.produto_cadastro_listagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ backend/src/main/resources/db/migration/V1__Create_Table_Product.sql
@@ -0,0 +1,10 @@
+CREATE TABLE supplier (
+    id varchar(255) NOT NULL,
+    name varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    price real NOT NULL,
+    email varchar(255) NOT NULL,
+    available boolean NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
