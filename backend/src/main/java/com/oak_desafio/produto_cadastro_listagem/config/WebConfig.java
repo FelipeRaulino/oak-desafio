b699598b5f40b3d780a0b43a288585d3e69384f2
Felipe Raulino <felipe.raaulino29@gmail.com>
2024-06-23 09:57:44 -0300

    feat: configure CORS to allow specific cross-origin requests in the backend

---
+++ backend/src/main/java/com/oak_desafio/produto_cadastro_listagem/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.oak_desafio.produto_cadastro_listagem.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns}")
+    private String[] originPatterns;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(originPatterns)
+                .allowCredentials(true);
+    }
+
+}
